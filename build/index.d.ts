9f81a2eee0805b82aa4c1c68b147806f1de55aec
gtskhadadze83 <gtskhadadze83@users.noreply.github.com>
2024-12-13 03:12:44 +0000

    deploy: ba064709d93dbaa3dc85da1b8e704ded75c11de4

--- build/index.d.ts
+++ build/index.d.ts
@@ -1,3 +1,3 @@
 import { AppConfig } from "./patchElectronApp";
 export declare const sodaPlayerBasicConfig: Omit<AppConfig, "patchContents">;
-export declare const patchSodaPlayer: (customPatchDirectory?: string | undefined) => Promise<void>;
+export declare const patchSodaPlayer: (customPatchDirectory?: string) => Promise<void>;
